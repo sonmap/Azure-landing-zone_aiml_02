7ae8377aaa9ef4d4764d39729feb167f7098116c
Son kyoungwon <78362051+sonmap@users.noreply.github.com>
2026-08-05 08:23:30 +0900

    Add platform variables

---
+++ landingzones/30-platform/variables.tf
@@ -0,0 +1,9 @@
+variable "subscription_id" { type = string }
+variable "location" { type = string default = "koreacentral" }
+variable "name_prefix" { type = string }
+variable "log_analytics_name" { type = string }
+variable "private_endpoint_subnet_key" { type = string default = "private-endpoints" }
+variable "tfstate_resource_group_name" { type = string }
+variable "tfstate_storage_account_name" { type = string }
+variable "tfstate_container_name" { type = string default = "tfstate" }
+variable "tags" { type = map(string) default = {} }
